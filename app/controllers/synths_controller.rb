fe8563d47d8c9df5719856387e0883d1fee52391
jbrasko <jbrasko0508@gmail.com>
2021-03-15 13:16:01 -0500

    add seed file

--- app/controllers/synths_controller.rb
+++ app/controllers/synths_controller.rb
@@ -1,2 +1,7 @@
 class SynthsController < ApplicationController
+    
+    def index
+        render json: Synth.all
+    end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,143 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Synth.create(
+    name: "Korg minilogue XD 4-voice Analog Synthesizer",
+    brand: "Korg",
+    image: "https://media.sweetwater.com/api/i/q-82__ha-beceb45518dd8602__hmac-b7f88c9def46a263cb17d08cde50fc837a22b58d/images/items/750/MinilogueXD-large.jpg",
+    price: 649.99,
+    short_desc: "4-voice Analog/Digital Synthesizer with 2 VCOs per Voice, Digital Multi-engine, Effects, 16-step Polyphonic Sequencer, 4 Voice Modes, and MIDI/Sync I/O",
+    long_desc: "With the Korg minilogue XD hybrid synthesizer, you’ve got a 4-voice analog synth bolstered by Korg’s digital multi-engine. Factor in the built-in effects, powerful sequencer, and micro tuning capability, and you have a full-featured sound design system at your fingertips. It’s based on the analog synth circuit from the minilogue, further refined for maximum sonic appeal. And with the digital multi-engine giving you access to four types of noise, a VPM/FM oscillator, and your own custom user oscillators, there’s a lifetime of creative potential to be explored with the Korg minilogue XD hybrid synthesizer.",
+    stock: 24
+)
+
+Synth.create(
+    name: "Korg MS-20 FS Full-size MS-20 Synthesizer (Green)",
+    brand: "Korg",
+    image: "https://media.sweetwater.com/api/i/q-85__ha-fc28fe9e71838c5d__hmac-679d3f86b4688384948df38856c8d2bc879ebfbf/images/items/1800/MS20FSGR-xlarge.jpg",
+    price: 1399.99,
+    short_desc: "37-key Classic Analog Monosynth with Patchbay, External Signal Processor, MIDI In, and USB - Green",
+    long_desc: "Released in 1978, the Korg MS-20 was an instant classic that would turn up in much of the synth-pop, fusion, and film music of the ’80s and ’90s. Recognizing the significance of their iconic semi-modular analog monosynth, Korg carried on the tradition in a full-sized reissue, a soft synth plug-in, and even a kit version. Now, Sweetwater is thrilled to present the full-sized, fully assembled MS-20. Available in four limited-edition colors, the MS-20 FS gives you all the synthy goodness of the FS, plus a number of enhancements serious synthesists have been clamoring for — including a highly playable, full-sized 37-note keyboard and two filter types from early- and late-model MS-20s. The MS-20 FS is the real deal, faithful to the original in every way. Today, over 40 years after its introduction, musicians can once again rock the legendary analog mojo of the Korg MS-20.",
+    stock: 4
+)
+
+Synth.create(
+    name: "Korg Prologue 49-key 8-voice Analog Synthesizer",
+    brand: "Korg",
+    image: "https://media.sweetwater.com/api/i/q-82__ha-bf10263824852da9__hmac-6b27863b170c281188431539a01843480f4147b2/images/items/750/Prologue08-large.jpg",
+    price: 999.99,
+    short_desc: "49-key, 8-voice Analog Synthesizer with 3 Oscillators per Voice, Filter with Drive and Adjustable Gain Compensation, Arpeggiator, and Dual Effects Section",
+    long_desc: "The Korg Prologue may be the modern analog polysynth you've been waiting for. Featuring 49 full-size keys and eight voices, plus Split, Layer, and Crossfade modes, Korg Prologue is primed for creating complex layers of sounds. Each voice boasts two analog oscillators plus a digital oscillator offering FM and wavetable synthesis. And with the ability to create your own oscillators and effects, Korg Prologue is one of the most customizable hardware synths we've ever seen at Sweetwater. With four different voice modes, an arpeggiator, a dual effects section, and 500 program locations for saving your sonic creations, the Korg Prologue is an undeniably powerful synth for stage and studio.",
+    stock: 2
+)
+
+Synth.create(
+    name: "Korg Volca Keys Analog Loop Synthesizer",
+    brand: "Korg",
+    image: "https://media.sweetwater.com/api/i/q-82__ha-e23aa550e9d997bd__hmac-1124fda249b148e353d86b1979fa6e9be26b5814/images/items/750/VolcaKeys-large.jpg",
+    price: 149.99,
+    short_desc: "Polyphonic Analog Synthesizer, with 3 x Self-tuning Oscillators, Built-in Delay Effect, Unison Mode, Ring Modulation, MIDI In, and Sync I/O",
+    long_desc: "Korg's Volca Keys analog synthesizer is a must-have for any fan of synths, packing huge analog tone and a surprisingly flexible interface into an ultra-affordable and portable package. This 3-note synth has self-tuning analog oscillators for stable pitch, and lets you stack notes to form chord stabs or combine them in unison for huge lead lines. Factor in the fat-sounding filter, delay effect, and a built-in arpeggiator, and you're well on your way to analog synth bliss. Best of all, you can store up to eight of your own patterns for recall anytime with the Korg Volca Keys.",
+    stock: 36
+)
+
+Synth.create(
+    name: "Moog Grandmother Semi-Modular Analog Synthesizer and Step Sequencer",
+    brand: "Moog",
+    image: "https://media.sweetwater.com/images/items/750/GRANDMother-large.jpg?v=b3b80f8c9fac7983",
+    price: 999.00,
+    short_desc: "32-key Semi-modular Analog Synthesizer with Spring Reverb, Arpeggiator, and Sequencer",
+    long_desc: "The Moog Grandmother semi-modular analog synthesizer had us at built-in spring reverb, and there is plenty more creative potential here than meets the eye. You can start playing and designing sounds immediately — no patching required. The onboard arpeggiator and sequencer are a blast to play with, and you can even save three sequences for instant recall anytime. And with its 32-note Fatar keybed, the Grandmother is one of the best-feeling compact analog synths you'll find. Whether you need it for stage or studio, the Moog Grandmother offers a fresh take on the classic Moog sound.",
+    stock: 8
+)
+
+Synth.create(
+    name: "Moog One 16-voice Analog Synthesizer",
+    brand: "Moog",
+    image: "https://media.sweetwater.com/api/i/q-85__ha-deacba6e75520c66__hmac-1fb41e1d9b5c1a168f3b5b2666bc1980b62b92bc/images/items/1800/MoogOne16-xlarge.jpg",
+    price: 8499.00,
+    short_desc: "61-key, 16-voice Programmable, Tri-timbral Analog Synthesizer with 3 Voltage-Controlled Oscillators, 2 Independent Analog Filters, 4 LFOs, 3 Envelope Generators, Arpeggiator, Sequencer, Onboard Effects and Eventide Reverb Suite, and CV I/O",
+    long_desc: "As the first polyphonic Moog synthesizer in more than three decades, Moog One spearheads a new era of analog synthesis. Handcrafted at the Moog factory in Asheville, North Carolina, Moog One is a programmable, tri-timbral analog synth featuring an intuitive tactile interface that transcends the boundary between instrument and artist, allowing you to achieve your musical goals unimpeded. And then, of course, there’s that legendary Moog sound. Moog analog circuits are renowned for their unrivaled punch and rich harmonics, and Moog One represents the definitive evolution of these classic circuits. Years of research went into this analog dream synth, and it shows in every aspect of its masterful design. Under your fingers, Moog One will exceed your every expectation as it inspires your musical creativity and opens portals to a vast sonic universe.",
+    stock: 2
+)
+
+Synth.create(
+    name: "Moog Mother-32 Semi-modular Eurorack Analog Synthesizer and Step Sequencer",
+    brand: "Moog",
+    image: "https://media.sweetwater.com/images/items/750/Mother32-large.jpg?v=e329b463c6de0d7d",
+    price: 649.00,
+    short_desc: "60HP Eurorack-format Modular Monophonic Synthesizer with Voltage-controlled Step Sequencer, Voltage-controlled Moog Ladder Filter, and MIDI Input",
+    long_desc: "Moog joins the Eurorack revolution with the semi-modular Mother-32 monosynth. This distinctly vintage-voiced, raw, powerful synthesizer boasts switchable lowpass and highpass Moog ladder filters (20Hz-20kHz), a voltage-controlled 32-step sequencer for self-contained programming, a MIDI input for external control and MIDI to CV conversion, and a 32-point 3.5mm patchbay for interconnectivity and extended synthesis capabilities. For those of us at Sweetwater who've jumped into this modern modular synth format, the Mother-32 is truly exciting news. And if you're looking to get into the Eurorack synth platform, you couldn't ask for a better starting point.",
+    stock: 8
+)
+
+Synth.create(
+    name: "Moog Matriarch Dark Semi-Modular Analog Synthesizer and Step Sequencer",
+    brand: "Moog",
+    image: "https://media.sweetwater.com/api/i/q-82__ha-0cd09b59523373e1__hmac-b24c29f1491b6142e1b2751ff2333c4934e993a7/images/items/750/MatriarchDk-large.jpg",
+    price: 1999.00,
+    short_desc: "49-key Semi-modular Paraphonic Analog Synthesizer, with Stereo Analog Delay, Arpeggiator, 256-step Sequencer, and 90 Modular Patch Points - Dark",
+    long_desc: "With paraphonic playability and an impressive 90 modular patch points, the Moog Matriarch semi-modular analog synthesizer is a sound-design powerhouse. On its own, Matriarch packs everything players love about Moog — rich oscillators, dual ladder filters, powerful arpeggiation and sequencing, and even stereo analog delay. And once you begin exploring its patch points with your modular synth environment, it's soon apparent that the Matriarch offers a lifetime of creative potential. Complete with a delightfully responsive 49-key Fatar keybed (with patchable velocity and aftertouch), the Moog Matriarch paraphonic analog synthesizer is the perfect way to establish — or expand — your own custom modular synth setup.",
+    stock: 12
+)
+
+Synth.create(
+    name: "Roland Jupiter-X 61-key Synthesizer",
+    brand: "Roland",
+    image: "https://media.sweetwater.com/api/i/q-82__ha-b19961ebb01bd443__hmac-9999dd337578d2bf5235d10460bd574e37b3e1c6/images/items/750/JupiterX-large.jpg",
+    price: 2579.99,
+    short_desc: "61-key Synthesizer with Aftertouch, I-Arpeggio, Onboard FX, Mic Input, AUX In, TS and Balanced XLR Outputs, MIDI, USB-to-Host, USB-to-Device, 2 Headphone Outputs, and 2 Pedal Jacks",
+    long_desc: "In 1978, when polysynths were the hot new thing, the Jupiter-4 put Roland on the map as a top synth maker. Since then, synthesizers bearing the Jupiter badge have proudly represented Roland's premier sound technologies and keyboard actions. Now Sweetwater is excited to present to you the Jupiter-X Synthesizer, which incorporates classic Roland design and tour-tough build quality with the beastly new ZEN-Core synth engine. The Jupiter-X meticulously re-creates coveted instruments from Roland's vaunted legacy of genre-defining synths, while blazing new synthesis frontiers. Featuring multiple layers, massive polyphony, and deep hands-on control, the Jupiter-X gives you the power to effortlessly craft and deploy huge, evolving soundscapes with warmth, punch, and shimmer.",
+    stock: 3
+)
+
+Synth.create(
+    name: "Roland JU-06A Boutique Series Juno Sound Module",
+    brand: "Roland",
+    image: "https://media.sweetwater.com/api/i/q-82__ha-686486757629b5cd__hmac-016dd3790b59ca1ce4531755b80521637351c6b8/images/items/750/JU06A-large.jpg",
+    price: 399.99,
+    short_desc: "4-voice Synthesizer Module and USB Audio Interface with 60/106 Modes, 1 LFO, 1 Oscillator, 2 Filters, 1 Envelope, 16-step Sequencer, Chorus, Delay, Built-in Speaker, and MIDI I/O",
+    long_desc: "Vintage Juno-60 and Juno-106 synthesizers are sought-after items — their warm sound, rich chorus effect, and hands-on creation capabilities are legendary. Now, courtesy of Roland’s JU-06A module, you can get both synths in one reliable, modern instrument. This module is tailor-made for electronic musicians. Use it with a sequencer via USB or MIDI, play it anywhere thanks to battery operation and a built-in speaker, or even use it as an audio interface for a simple production setup. What’s more, it looks like the real deal by virtue of its classic color scheme and styling. If you’re a vintage synth aficionado the JU-06A is a must-have item.",
+    stock: 18
+)
+
+Synth.create(
+    name: "Roland SH-01A Boutique Series Synthesizer with Sequencer",
+    brand: "Roland",
+    image: "https://media.sweetwater.com/api/i/q-82__ha-d7c0faaaa51762cc__hmac-d2adeba4a7b151b69b28fff7c99a47461fc9c503/images/items/750/SH01A-large.jpg",
+    price: 399.99,
+    short_desc: "Boutique Series 4-voice Synthesizer Module with 1 VCO, 1 VCF, 1 VCA, 1 LFO, 100-note Step Sequencer, Arpeggiator, 64 Preset Patches, and CV/Gate Output",
+    long_desc: "The SH-01A reimagines Roland's venerable SH-101 synthesizer as a Boutique Series module. Just like the original, the SH-01A generates distinctive bass sounds and bubbly analog effects using a simple 1-VCO, 1-VCF, 1-VCA, 1-LFO design. But thanks to modern innovations, you can now perform full chords with 4-voice polyphony, greatly expanding your sound palette. And whether you craft a throbbing bass line, buzzing lead, or otherworldly sound effect, you can store up to 64 preset patches for instant recall. Complete with 100-note step sequencer, arpeggiator, and selectable advanced LFO, the SH-01A is a must-have for any synth enthusiast.",
+    stock: 6
+)
+
+Synth.create(
+    name: "Dave Smith Instruments Prophet Rev2-08 8-voice Analog Synthesizer",
+    brand: "Dave Smith Instruments",
+    image: "https://media.sweetwater.com/api/i/q-82__ha-af10233b5aeb206a__hmac-5fc226bc76c4b28f3c520d59814498d02a855e67/images/items/750/REV2-08-large.jpg",
+    price: 1499.00,
+    short_desc: "61-key 8-voice Analog Synthesizer with Per-voice Dual DCOs, Sub-oscillator, and 2/4-pole Resonant Lowpass Curtis Filters; Bi-timbral Operation, Waveshape Modulation, Onboard FX Engine, Polyphonic Step Sequencer, Aftertouch, OLED Display, and USB",
+    long_desc: "Dave Smith Instruments ups the ante (again) with the Prophet Rev2, a 61-key analog synthesizer that takes over where the legendary Prophet '08 left off. Offered in eight-voice or 16-voice version (this is the 8-voice version), the Rev2 is an absolute beast with dual DCOs, a sub-oscillator, and a 2/4-pole resonant lowpass Curtis filter per voice. Other features include bi-timbral operation, waveshape modulation, a beefy onboard FX engine, an advanced polyphonic step sequencer, a crisp OLED display, and USB. The premium 5-octave, semi-weighted keyboard with velocity and channel aftertouch feels just right. Synth geeks here at Sweetwater are excited about Dave Smith Instruments' Prophet Rev2. Dave has delivered another killer synth — and it's an amazing value.",
+    stock: 10
+)
+
+Synth.create(
+    name: "Dave Smith Instruments OB-6 Module 6-voice Polyphonic Analog Synthesizer Module",
+    brand: "Dave Smith Instruments",
+    image: "https://media.sweetwater.com/api/i/q-82__ha-ae6a37694dbecfab__hmac-645659a5cb7d0309498042f75846a2aeb21a56fc/images/closeup/750-OB6mod_detail1.jpg",
+    price: 2299.00,
+    short_desc: "6-voice Analog Polysynth Module with 2 Oscillators and Sub Oscillator Per Voice, State-variable Filter, 64-step Polyphonic Step Sequencer, and Poly Chain Feature",
+    long_desc: "What if two legendary synthesizer designers got together and built an analog polysynth using today's state-of-the-art technology? If you've just pinched yourself to see if you're dreaming, Sweetwater can assure you: you're not. Dave Smith Instruments OB-6 Module is a 6-voice, all-analog, desktop synth inspired by Tom Oberheim's classic SEM circuits from the 1970s. Tom and Dave packed the OB-6 Module with everything on your wish list. With two voltage-controlled oscillators and sub oscillator per voice, a state-variable filter, 64-step polyphonic step sequencer, twin digital effects engines, and more, Dave Smith Instruments OB-6 Module delivers fat, punchy, in-your-face sonics that will power your music to new heights.",
+    stock: 4
+)
+
+Synth.create(
+    name: "Dave Smith Instruments OB-6 6-voice Polyphonic Analog Synthesizer",
+    brand: "Dave Smith Instruments",
+    image: "https://media.sweetwater.com/api/i/q-82__ha-f6ec9216d5639b39__hmac-b142a179bd56b41eec1ec34b56fc35a32100e81d/images/items/750/OB6-large.jpg",
+    price: 2999.00,
+    short_desc: "49-key, 6-voice Analog Polysynth with 2 Oscillators and a Sub Oscillator Per Voice, State-variable Filter, 64-step Polyphonic Step Sequencer, and Semi-weighted, Velocity- and Aftertouch-enabled Keyboard",
+    long_desc: "What if two legendary synthesizer designers got together and built an analog polysynth using today's state-of-the-art technology? If you've just pinched yourself to see if you're dreaming, Sweetwater can assure you: you're not. Dave Smith Instruments OB-6 is a 6-voice, all-analog, 49-key synth inspired by Tom Oberheim's classic SEM circuits from the 1970s. Tom and Dave packed the OB-6 with everything on your wish list. With two voltage-controlled oscillators and sub oscillator per voice, a state-variable filter, 64-step polyphonic step sequencer, twin digital effects engines, and more, Dave Smith Instruments OB-6 delivers fat, punchy, in-your-face sonics that will power your music to new heights.",
+    stock: 6
+)
