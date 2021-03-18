e82a7c3b4eebaeac775c6e748fa2f2571b56be5c
jbrasko <jbrasko0508@gmail.com>
2021-03-18 16:58:22 -0500

    add review create action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,14 @@
 class ReviewsController < ApplicationController
 
-    
+    def create
+        review = logged_in_user.reviews.create(review_params)
+        render json: review
+    end
+
+    private
+
+    def review_params
+        params.permit(:content, :rating, :synth_id)
+    end
+
 end
